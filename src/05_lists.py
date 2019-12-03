1af702c223d6697505e5527fc0e35f4e0ae3bb26
Jason Prince <jasoncprince@gmail.com>
2019-12-02 19:20:19 -0700

    finished first half of list

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,35 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x)
+
+x.append(4)
+print("append",x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+# for item in y:
+#     x.append(item)
+
+x.extend(y)
+print("extend",x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+x.remove(8)
+print("remove",x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+x.insert(5, 99)
+print("insert",x)
 
 # Print the length of list x
 # YOUR CODE HERE 
 
+print("length",len(x))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+for num in x:
+    print("times 1000",num*1000)
